34d4541d83dc5e22b85dd8c6de8d2e0ad99fe64b
cxxxr <g23tlm@gmail.com>
2021-06-03 18:20:29 +0900

    add cl-yaml test

--- tests/parser.lisp
+++ tests/parser.lisp
@@ -1,4 +1,8 @@
 (in-package :openapi-parser-tests)
 
+(deftest yaml
+  (ok (gethash "200" (yaml:parse "{'200': 'ok'}")))
+  (ok (gethash 200 (yaml:parse "{200: 'ok'}"))))
+
 (deftest parse-file
   )
