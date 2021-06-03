c051d5056ada5473719d375fe1b9ab81668c8b26
cxxxr <g23tlm@gmail.com>
2021-06-03 20:27:03 +0900

    add export

--- package.lisp
+++ package.lisp
@@ -6,10 +6,70 @@
   (:use :cl :alexandria))
 
 (defpackage :openapi-parser/schema/3.0.1
-  (:use :cl :alexandria))
+  (:use :cl :alexandria)
+  (:export :<open-api>
+           :<info>
+           :<contact>
+           :<license>
+           :<server>
+           :<server-variable>
+           :<components>
+           :<paths>
+           :<path-item>
+           :<operation>
+           :<external-documentation>
+           :<parameter>
+           :<request-body>
+           :<media-type>
+           :<encoding>
+           :<responses>
+           :<response>
+           :<callback>
+           :<example>
+           :<link>
+           :<header>
+           :<tag>
+           :<reference>
+           :<schema>
+           :<discriminator>
+           :<xml>
+           :<security-scheme>
+           :<oauth-flow>
+           :<o-auth-flow>
+           :<security-requirement>))
 
 (defpackage :openapi-parser/schema/3.1.0
-  (:use :cl :alexandria))
+  (:use :cl :alexandria)
+  (:export :<open-api>
+           :<info>
+           :<contact>
+           :<license>
+           :<server>
+           :<server-variable>
+           :<components>
+           :<paths>
+           :<path-item>
+           :<operation>
+           :<external-documentation>
+           :<parameter>
+           :<request-body>
+           :<media-type>
+           :<encoding>
+           :<responses>
+           :<response>
+           :<callback>
+           :<example>
+           :<link>
+           :<header>
+           :<tag>
+           :<reference>
+           :<schema>
+           :<discriminator>
+           :<xml>
+           :<security-scheme>
+           :<oauth-flow>
+           :<o-auth-flow>
+           :<security-requirement>))
 
 (cl-package-locks:lock-package :openapi-parser)
 (cl-package-locks:lock-package :openapi-parser/schema)
